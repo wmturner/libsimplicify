4f68c2f392f07bf5b97130dffc068e7cc1405c60
root <root@simplicify-node1.simplicify.com>
2015-10-15 20:06:31 -0700

    More changes

--- controller/libsimplicify.py
+++ controller/libsimplicify.py
@@ -53,3 +53,26 @@ class provisioner:
 
         if type == "baremetal":
             client.write('/build/{}/{}'.format(type,jobID), json.dumps(job))
+
+
+
+    def pollBuildQueue(self):
+        print "hello"
+        vote = random.randrange(0, 1000, 3)
+        queue = client.read('/build/{}'.format(NODE_TYPE), recursive = True, wait = True)
+
+        jobID = queue.key.rsplit('/',1)[1]
+
+        client.write('/vote/{}/{}/{}'.format(NODE_TYPE, jobID, host), vote)
+        vote_queue = client.read('/vote/{}/{}'.format(NODE_TYPE, jobID), recursive = True)
+
+        quorum_dict = {}
+
+        for vote in vote_queue._children:
+            quorum_dict.update({ vote['key'].rsplit('/',1)[1]: vote['value'] })
+
+        winner = max(quorum_dict.iteritems(), key=operator.itemgetter(1))[0]
+
+        if winner==host:
+            print "I win!"
+        return 0
