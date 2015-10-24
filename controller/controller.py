be97b9d116a8ea51dd5c8dba181205bbb8fe21b8
root <root@simplicify-node1.simplicify.com>
2015-10-24 12:21:16 -0700

    Added s3 methods and connection setup

--- controller/controller.py
+++ controller/controller.py
@@ -8,7 +8,8 @@ with open('config.json') as data_file:
     config = json.load(data_file)
 
 simplicify_instance = simplicify('controller1.simplicify.com', config)
-simplicify_instance.connect()
+simplicify_instance.connect_etcd()
+simplicify_instance.connect_s3()
 #client = etcd.Client(host='controller1.simplicify.com', port=2379)
 cmdObj = commands(simplicify_instance, config)
 
@@ -17,5 +18,6 @@ test_settings = { "cheffile": "http://controller1.simplicify.com/cheffiles/simpl
 
 #cmdObj.queue_build('baremetal', '5', 'chef', test_settings, 'testing123')
 #cmdObj.echo_build('baremetal', '4', '<3u evan')
-cmdObj.rm_build('baremetal', '4')
+#cmdObj.rm_build('baremetal', '4')
+cmdObj.ls_bkts()
 
--- libsimplicify.py
+++ libsimplicify.py
@@ -7,6 +7,9 @@ import random
 import socket
 import operator
 import time
+import boto
+import boto.s3.connection
+
 NODE_TYPE="baremetal"
 
 class simplicify:
@@ -14,15 +17,37 @@ class simplicify:
         self.clusterFQDN = clusterFQDN
         self.SimplicifyConfig = SimplicifyConfig
 
-    def connect(self):
-        self.client = etcd.Client(host='controller1.simplicify.com', port=2379)
-        return self.client
+    def connect_etcd(self):
+        
+        host_etcd = self.SimplicifyConfig['etcd']['uri']
+        port_etcd = self.SimplicifyConfig['etcd']['port']
 
+        self.client_etcd = etcd.Client(host=host_etcd, port=port_etcd)
+
+        return self.client_etcd
+
+    def connect_s3(self):
+
+        access_key = self.SimplicifyConfig['s3']['access_key']
+        secret_key = self.SimplicifyConfig['s3']['secret_key']
+        host_s3 = self.SimplicifyConfig['s3']['host']
+        security_setting = (self.SimplicifyConfig['s3']['is_secure'] == "True")
+
+        self.client_s3 = boto.connect_s3(
+        aws_access_key_id = access_key,
+        aws_secret_access_key = secret_key,
+        host = host_s3,
+        is_secure=security_setting,
+        calling_format = boto.s3.connection.OrdinaryCallingFormat(),
+        )
+
+        return self.client_s3
 
 class commands:
     def __init__(self, simplicify, provisioner_type):
         self.provisioner_type = provisioner_type
-        self.client = simplicify.client
+        self.client_etcd = simplicify.client_etcd
+        self.client_s3 = simplicify.client_s3
         self.config = simplicify.SimplicifyConfig
         self.hostname = socket.gethostname()
 
@@ -58,23 +83,24 @@ class commands:
                 "appliance_name": self.appliance_name }
 
         if type == "baremetal":
-            self.client.write('/build/{}/{}'.format(type,jobID), json.dumps(job))
+            self.client_etcd.write('/build/{}/{}'.format(type,jobID), json.dumps(job))
 
 
     def rm_build(self, type, jobID):
         
         try: 
-            self.client.read("/build/{}/{}".format(type, jobID))
-            self.client.delete("/build/{}/{}".format(type, jobID))
+            self.client_etcd.read("/build/{}/{}".format(type, jobID))
+            self.client_etcd.delete("/build/{}/{}".format(type, jobID))
         except etcd.EtcdKeyNotFound:
             print "key not found"
         return
 
     def echo_build(self, type, jobID, value):
         try:
-            self.client.read("/build/{}/{}".format(type, jobID))
+            self.client_etcd.read("/build/{}/{}".format(type, jobID))
         except etcd.EtcdKeyNotFound:
-            self.client.write('/build/{}/{}'.format(type,jobID), value)
+            print "Key not found"
+            self.client_etcd.write('/build/{}/{}'.format(type,jobID), value)
         
         
 
@@ -82,7 +108,7 @@ class commands:
        # print "Beginning Polling loop"
 
         # Execute a recursive query to etcd and only show a single new value after the request to the cluster was made:
-        self.queue = self.client.read('/build/{}'.format(NODE_TYPE), recursive = True)._children
+        self.queue = self.client_etcd.read('/build/{}'.format(NODE_TYPE), recursive = True)._children
         
         pick = random.choice(self.queue)        
         print pick['key']
@@ -95,7 +121,7 @@ class commands:
             if winner==self.hostname:
                 try:
                     # Double check to make sure another node has not claimed winner for the jobID
-                    out_of_syncWinner = self.client.read('/winner/{}/{}/{}'.format(NODE_TYPE, jobID, self.hostname))
+                    out_of_syncWinner = self.client_etcd.read('/winner/{}/{}/{}'.format(NODE_TYPE, jobID, self.hostname))
                     print "WINNER ALREADY SELECTED, YOU ARE OUT OF SYNC"
 
                 # During good cluster behavior, the below condition should be triggered
@@ -103,11 +129,11 @@ class commands:
 
                     # If node truly is the winner, then write it to the winner queue for record
                     # Inform the cluster that the provisioning job is in progress
-                    self.client.write('/winner/{}/{}/{}'.format(NODE_TYPE, jobID, self.hostname), pick['value'])
-                    self.client.write('/inprogress/{}/{}'.format(NODE_TYPE, jobID), "True")
+                    self.client_etcd.write('/winner/{}/{}/{}'.format(NODE_TYPE, jobID, self.hostname), pick['value'])
+                    self.client_etcd.write('/inprogress/{}/{}'.format(NODE_TYPE, jobID), "True")
 
                     # Delete the job from the build queue
-                    self.client.delete(pick['key'])
+                    self.client_etcd.delete(pick['key'])
         #        self.processJob()
                     print "Win"
                 #os.chroot("/simplicify/chroot")
@@ -121,11 +147,11 @@ class commands:
         # A vote is an integer value between 1 and 1000, and the cluster member with the highest number wins:
         vote = random.randrange(100, 1000, 3)
         # Write the worker's vote to the appropriate etcd folder:
-        self.client.write('/vote/{}/{}/{}'.format(NODE_TYPE, jobID, self.hostname), vote)
+        self.client_etcd.write('/vote/{}/{}/{}'.format(NODE_TYPE, jobID, self.hostname), vote)
 
         # Wait a minute until all workers have had an opportunity to vote:
         time.sleep(1)
-        vote_queue = self.client.read('/vote/{}/{}'.format(NODE_TYPE, jobID), recursive = True)
+        vote_queue = self.client_etcd.read('/vote/{}/{}'.format(NODE_TYPE, jobID), recursive = True)
 
         # Create a quorum dictionary used to store all votes from the various members and determine which worker is the winner
         quorum_dict = {}
@@ -135,6 +161,9 @@ class commands:
         winner = max(quorum_dict.iteritems(), key=operator.itemgetter(1))[0]
         return winner
     
+ #   def put_chroot(self):
+ #       chroot_id
+ #       self.client_etcd.write('/artifacts/chroots/{}/'.format(, jobID, self.hostname), vote) 
     
     def provisioners(self):
         
@@ -143,4 +172,12 @@ class commands:
             call(["chef-solo", "-o", "nginx"])
             
             return
+        return
+    
+    def ls_bkts(self):
+        for bucket in self.client_s3.get_all_buckets():
+            print "{name}\t{created}".format(
+                    name = bucket.name,
+                    created = bucket.creation_date,
+            )
         return
